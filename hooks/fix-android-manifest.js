350ded767cea9cd99a2a353a0fddad6df89c06ee
ancipov <2110267+ancipov@users.noreply.github.com>
2026-06-15 19:59:25 +0300

    added fix android manifest

--- hooks/fix-android-manifest.js
+++ hooks/fix-android-manifest.js
@@ -3,6 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 
+const AD_ACTIVITY =
+    '<activity android:configChanges="keyboard|keyboardHidden|orientation|screenLayout|uiMode|screenSize|smallestScreenSize" android:excludeFromRecents="true" android:exported="true" android:name="com.google.android.gms.ads.AdActivity" android:noHistory="true" tools:replace="android:exported" />';
+
+const AD_SERVICES_PROPERTY =
+    '<property android:name="android.adservices.AD_SERVICES_CONFIG" android:resource="@xml/gma_ad_services_config" tools:replace="android:resource" />';
+
 function ensureToolsNamespace(manifest) {
     if (manifest.includes('xmlns:tools="http://schemas.android.com/tools"')) {
         return manifest;
@@ -30,38 +36,17 @@ module.exports = function (context) {
     manifest = ensureToolsNamespace(manifest);
 
     const adActivityPattern =
-        /<activity[^>]*android:name="com\.google\.android\.gms\.ads\.AdActivity"[^>]*\/?>/g;
-    const adActivities = manifest.match(adActivityPattern) || [];
-
-    if (adActivities.length > 1) {
-        const adActivityEntry =
-            '<activity android:configChanges="keyboard|keyboardHidden|orientation|screenLayout|uiMode|screenSize|smallestScreenSize" android:excludeFromRecents="true" android:exported="true" android:name="com.google.android.gms.ads.AdActivity" android:noHistory="true" tools:replace="android:exported" />';
-
-        manifest = manifest.replace(adActivityPattern, '');
-        manifest = manifest.replace(
-            '</application>',
-            `        ${adActivityEntry}\n    </application>`
-        );
-    } else if (adActivities.length === 1 && !adActivities[0].includes('tools:replace')) {
-        manifest = manifest.replace(
-            adActivityPattern,
-            adActivities[0].replace(
-                '<activity',
-                '<activity tools:replace="android:exported"'
-            )
-        );
-    }
-
-    const adServicesProperty =
-        '<property android:name="android.adservices.AD_SERVICES_CONFIG" android:resource="@xml/gma_ad_services_config" tools:replace="android:resource" />';
+        /\s*<activity[^>]*android:name="com\.google\.android\.gms\.ads\.AdActivity"[^>]*\/?>\s*/g;
+    manifest = manifest.replace(adActivityPattern, '\n');
 
     manifest = manifest.replace(
         /<property[^>]*android:name="android\.adservices\.AD_SERVICES_CONFIG"[^>]*\/?>\s*/g,
         ''
     );
+
     manifest = manifest.replace(
         '</application>',
-        `        ${adServicesProperty}\n    </application>`
+        `        ${AD_ACTIVITY}\n        ${AD_SERVICES_PROPERTY}\n    </application>`
     );
 
     if (manifest !== original) {
